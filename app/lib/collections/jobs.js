371dfeb4b1b85e044393b2cec0f7fb0a8e3f4fd7
xcv58 <chenyihonglove@gmail.com>
2016-04-26 16:31:44 -0400

    add Meteor.testClone; nodegit doesn't work with Meteor

--- app/lib/collections/jobs.js
+++ app/lib/collections/jobs.js
@@ -2,4 +2,6 @@ import { JobCollection as jobCollection } from 'meteor/vsivsi:job-collection';
 
 const repoJobs = jobCollection('repos');
 
+repoJobs.startJobServer();
+
 export { repoJobs };
--- app/server/methods/repos.js
+++ app/server/methods/repos.js
@@ -3,11 +3,21 @@ import { check } from 'meteor/check';
 import { Job } from 'meteor/vsivsi:job-collection';
 import { repoJobs } from '../../lib/collections';
 
+Meteor.testClone = () => {
+  Meteor.call('repos.add', {
+    name: 'xcv58/startup-tools',
+    url: 'https://github.com/xcv58/startup-tools',
+  });
+};
+
 export default function () {
   Meteor.methods({
     'repos.add'(repo) {
-      check(repo, String);
-      const job = new Job(repoJobs, 'new', { repo });
+      check(repo, {
+        name: String,
+        url: String,
+      });
+      const job = new Job(repoJobs, 'clone', { ...repo });
       job.priority('normal')
       .retry({
         retries: 5,
