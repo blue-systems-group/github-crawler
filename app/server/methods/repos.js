18fd6bf14ea83fdb7b2e5e0f95ca2c99759478b4
xcv58 <chenyihonglove@gmail.com>
2016-04-27 16:25:59 -0400

    add cloneAll, TODO: add repos collection to maintain existing repos

--- app/server/methods/repos.js
+++ app/server/methods/repos.js
@@ -1,13 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Job } from 'meteor/vsivsi:job-collection';
-import { repoJobs } from '../../lib/collections';
+import { repoJobs, Searchs } from '../../lib/collections';
 
 Meteor.testClone = () => {
   Meteor.call('repos.add', {
     name: 'xcv58/startup-tools',
     url: 'https://github.com/xcv58/startup-tools',
   });
+};
+
+Meteor.cloneAll = () => {
+  const cursor = Searchs.find();
+  for (const search of cursor.fetch()) {
+    for (const item of search.items) {
+      const { repository: { full_name: name, html_url: url } } = item;
+      console.log(name, url);
+      Meteor.call('repos.add', { name, url });
+    }
+  }
 };
 
 repoJobs.events.on('jobDone', message => {
