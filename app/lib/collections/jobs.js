4e293212844535beedf2ccc7db2972a6da06b7e8
xcv58 <chenyihonglove@gmail.com>
2016-04-27 10:15:20 -0400

    fix call jc.allow() from client

--- app/lib/collections/jobs.js
+++ app/lib/collections/jobs.js
@@ -2,27 +2,4 @@ import { JobCollection as jobCollection } from 'meteor/vsivsi:job-collection';
 
 const repoJobs = jobCollection('repos');
 
-repoJobs.startJobServer();
-
-repoJobs.allow({
-  admin() {
-    return true;
-  },
-  manager() {
-    return true;
-  },
-  jobRerun() {
-    return true;
-  },
-  jobSave() {
-    return true;
-  },
-  getWork() {
-    return true;
-  },
-  worker() {
-    return true;
-  },
-});
-
 export { repoJobs };
--- app/server/publications/jobs.js
+++ app/server/publications/jobs.js
@@ -2,7 +2,34 @@ import { repoJobs } from '../../lib/collections';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
+const initJobs = () => {
+  repoJobs.startJobServer();
+
+  repoJobs.allow({
+    admin() {
+      return true;
+    },
+    manager() {
+      return true;
+    },
+    jobRerun() {
+      return true;
+    },
+    jobSave() {
+      return true;
+    },
+    getWork() {
+      return true;
+    },
+    worker() {
+      return true;
+    },
+  });
+};
+
 export default () => {
+  initJobs();
+
   Meteor.publish('allJobs', (userId) => {
     check(userId, null);
     const selector = {};
